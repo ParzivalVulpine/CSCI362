bf6334b68b975e41949fd1a17a531eebb89edc50
“Daniel <mrdaniomi36@gmail.com>
2023-09-08 11:53:30 -0500

    Class Lecture on Assignment 2

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -1,5 +1,6 @@
 import torch
 import csv
+import matplotlib.pyplot as plt
 with open('assign2.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     next(csvfile)  # skip the first line
@@ -8,3 +9,12 @@ with open('assign2.csv') as csvfile:
         xs.append(float(row[0]))
         ys.append(float(row[1]))
 xs, ys = torch.tensor(xs), torch.tensor(ys)
+
+regTensor = torch.empty(1, 60)
+for i in range(60):
+    regTensor[1][i] = i
+    regTensor[0][i] = 1
+print(regTensor)
+
+plt.scatter(xs.numpy(), ys.numpy())
+plt.show()
