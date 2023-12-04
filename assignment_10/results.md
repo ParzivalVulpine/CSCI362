faa6c3ed943dc13fbc96bf2a92c71ca6f3d971d2
Daniel Gaitan <mrdaniomi36@gmail.com>
2023-12-04 13:25:07 -0600

    Added weights to the results.md in assignment_10

--- assignment_10/results.md
+++ assignment_10/results.md
@@ -1,2 +1,12 @@
 ![Alt text](image.png)
 ![Alt text](image-1.png)
+
+# Weights Per Layer
+1. Layer 0: 400
+2. Layer 1: 300
+3. Layer 2: 200
+4. Layer 3: 100
+5. Layer 4: 10
+
+Therefore the total amount of weights will be (counting bias): 401 * 301 * 201 * 101 * 10 
+which totals to 24,503,510,010 (a lot) 
--- assignment_11/main.py
+++ assignment_11/main.py
@@ -3,32 +3,27 @@ import torch.nn as nn
 from skimage import io
 import du.lib as dulib
 
-torch.cuda.is_available = lambda : False
+torch.cuda.is_available = lambda: False
 
 # read in all the digits
 digits = io.imread('numbers.png')
-xss = torch.Tensor(5000,400)
+xss = torch.Tensor(5000, 400)
 idx = 0
-for i in range(0, 1000, 20):
-  for j in range(0, 2000, 20):
-    xss[idx] = torch.Tensor((digits[i:i+20,j:j+20]).flatten())
-    idx = idx + 1
-
 
 # generate yss to hold the correct classification for each example
 yss = torch.LongTensor(len(xss))
 for i in range(len(yss)):
-  yss[i] = i//500
+    yss[i] = i // 500
 
 xss, xss_means = dulib.center(xss)
 xss, xss_stds = dulib.normalize(xss)
 
 indices = torch.randperm(len(xss))
 xss = xss[indices]
-yss = yss[indices] # coherently randomize the data
-xss_train = xss[:4000] 
+yss = yss[indices]  # coherently randomize the data
+xss_train = xss[:4000]
 yss_train = yss[:4000]
-xss_test = xss[4000:] 
+xss_test = xss[4000:]
 yss_test = yss[4000:]
 
 
@@ -36,10 +31,10 @@ class LogSoftmaxModel(nn.Module):
 
     def __init__(self):
         super(LogSoftmaxModel, self).__init__()
-        self.layer1 = nn.Linear(400,300)
-        self.layer2 = nn.Linear(300,200)
-        self.layer3 = nn.Linear(200,100)
-        self.layer4 = nn.Linear(100,10)
+        self.layer1 = nn.Linear(400, 300)
+        self.layer2 = nn.Linear(300, 200)
+        self.layer3 = nn.Linear(200, 100)
+        self.layer4 = nn.Linear(100, 10)
 
     def forward(self, x):
         x = self.layer1(x)
@@ -51,23 +46,24 @@ class LogSoftmaxModel(nn.Module):
         x = self.layer4(x)
         return torch.log_softmax(x, dim=1)
 
+
 model = LogSoftmaxModel()
 
 criterion = nn.NLLLoss()
 
 model = dulib.train(
-  model=model,
-  crit=criterion,
-  train_data=(xss_train, yss_train),
-  valid_data=(xss_test, yss_test),
-  learn_params={"lr": 0.00025, "mo":0.952},
-  bs=25,
-  epochs=100,
-  graph=1
+    model=model,
+    crit=criterion,
+    train_data=(xss_train, yss_train),
+    valid_data=(xss_test, yss_test),
+    learn_params={"lr": 0.00025, "mo": 0.952},
+    bs=25,
+    epochs=100,
+    graph=1
 )
 
 pct_training = dulib.class_accuracy(model, (xss_train, yss_train), show_cm=True)
-print(f"Percentage correct on training data: {100*pct_training:.2f}")
+print(f"Percentage correct on training data: {100 * pct_training:.2f}")
 
 pct_testing = dulib.class_accuracy(model, (xss_test, yss_test), show_cm=True)
-print(f"Percentage correct on testing data: {100*pct_testing:.2f}")
+print(f"Percentage correct on testing data: {100 * pct_testing:.2f}")
