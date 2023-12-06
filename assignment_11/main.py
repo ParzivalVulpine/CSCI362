eecf17f0ee873f929eb01873c563cd3f6f261cec
Daniel Gaitan <mrdaniomi36@gmail.com>
2023-12-06 14:41:02 -0600

    Added second meta layer to assignment 11

--- assignment_11/main.py
+++ assignment_11/main.py
@@ -3,52 +3,61 @@ import torch.nn as nn
 from skimage import io
 import du.lib as dulib
 
-torch.cuda.is_available = lambda: False
-
-# read in all the digits
 digits = io.imread('numbers.png')
-xss = torch.Tensor(5000, 400)
+xss = torch.Tensor(5000, 20, 20)
 idx = 0
+for i in range(0, 1000, 20):
+    for j in range(0, 2000, 20):
+        xss[idx] = torch.Tensor((digits[i:i + 20, j:j + 20]))
+        idx = idx + 1
 
-# generate yss to hold the correct classification for each example
 yss = torch.LongTensor(len(xss))
 for i in range(len(yss)):
     yss[i] = i // 500
 
-xss, xss_means = dulib.center(xss)
-xss, xss_stds = dulib.normalize(xss)
-
 indices = torch.randperm(len(xss))
 xss = xss[indices]
-yss = yss[indices]  # coherently randomize the data
+yss = yss[indices]
 xss_train = xss[:4000]
 yss_train = yss[:4000]
 xss_test = xss[4000:]
 yss_test = yss[4000:]
 
+xss_train, xss_means = dulib.center(xss_train)
+xss_train, xss_stds = dulib.normalize(xss_train)
 
-class LogSoftmaxModel(nn.Module):
-
-    def __init__(self):
-        super(LogSoftmaxModel, self).__init__()
-        self.layer1 = nn.Linear(400, 300)
-        self.layer2 = nn.Linear(300, 200)
-        self.layer3 = nn.Linear(200, 100)
-        self.layer4 = nn.Linear(100, 10)
-
-    def forward(self, x):
-        x = self.layer1(x)
-        x = torch.relu(x)
-        x = self.layer2(x)
-        x = torch.relu(x)
-        x = self.layer3(x)
-        x = torch.relu(x)
-        x = self.layer4(x)
-        return torch.log_softmax(x, dim=1)
+xss_test, _ = dulib.center(xss_test, xss_means)
+xss_test, _ = dulib.normalize(xss_test, xss_stds)
 
 
-model = LogSoftmaxModel()
+class ConvolutionalModel(nn.Module):
 
+  def __init__(self):
+    super(ConvolutionalModel, self).__init__()
+    self.meta_layer1 = nn.Sequential(
+        nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding = 2),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size = 2, stride = 2, padding = 0)
+    )
+    self.meta_layer2 = nn.Sequential(
+        nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+    )
+    self.fc_layer1 = nn.Linear(800,10)
+
+  def forward(self, xss):
+    xss = torch.unsqueeze(xss, dim=1)
+    xss = self.meta_layer1(xss)
+    xss = self.meta_layer2(xss)
+    xss = torch.reshape(xss, (-1, 800))
+    xss = self.fc_layer1(xss)
+    return torch.log_softmax(xss, dim=1)
+
+# create an instance of the model class
+model = ConvolutionalModel()
+
+# set the criterion
 criterion = nn.NLLLoss()
 
 model = dulib.train(
@@ -56,7 +65,7 @@ model = dulib.train(
     crit=criterion,
     train_data=(xss_train, yss_train),
     valid_data=(xss_test, yss_test),
-    learn_params={"lr": 0.00025, "mo": 0.952},
+    learn_params={"lr": 0.00021, "mo": 0.956},
     bs=25,
     epochs=100,
     graph=1
